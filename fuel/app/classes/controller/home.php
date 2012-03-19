755925ec11452de766d59f5b9c3a153f72ad96a7
Andrew Wheal <sicoanimal@hotmail.com>
2012-03-19 00:21:11 +0000

    added repos page

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -14,6 +14,13 @@ class Controller_Home extends Controller_Template {
 		$this->template->content = View::forge('home/index');
 	}
 
+	public function action_repos() {
+		\SicoNav\Breadcrumbs::instance()->add('Repos', '/home/repos');
+		$this->template->title = 'Repos | GitHub Manager';
+		$this->template->content = View::forge('home/repos');
+		$this->template->content->repos = \GitHub\Repos::organisation('propcom')->execute();
+	}
+
 	public function action_404() {
 		$this->template->content = View::forge('404');
 	}
---
+++ fuel/app/views/home/repos.php
@@ -0,0 +1,5 @@
+<header class="page-header">
+	<h1>GitHub Repositories</h1>
+</header>
+
+<?= \GitHub\Repos::organisation('propcom')->render() ?>
