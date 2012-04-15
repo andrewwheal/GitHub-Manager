0e08932c793b6a8718e8504ae2fb7f9960ccfd3f
Andrew Wheal <sicoanimal@hotmail.com>
2012-04-15 01:33:52 +0100

    added jQuery to template

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -8,6 +8,7 @@
 		<?= Asset::css('bootstrap.min.css') ?>
 		<?= Asset::css('style.css') ?>
 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<?= Asset::js('bootstrap.min.js') ?>
 	</head>
 
