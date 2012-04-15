21eb7707fc459271c3366881745da7a406cbf486
Andrew Wheal <sicoanimal@hotmail.com>
2012-04-15 01:28:14 +0100

    moved repos action to it's own controller

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -5,20 +5,12 @@ class Controller_Home extends Controller_Template {
 	public function before() {
 		parent::before();
 
-		Package::load('siconav');
 		\SicoNav\Breadcrumbs::forge()->add('Home', '/');
 	}
 
 	public function action_index() {
 		$this->template->title = 'GitHub Manager';
 		$this->template->content = View::forge('home/index');
-	}
-
-	public function action_repos() {
-		\SicoNav\Breadcrumbs::instance()->add('Repos', '/home/repos');
-		$this->template->title = 'Repos | GitHub Manager';
-		$this->template->content = View::forge('home/repos');
-		$this->template->content->repos = \GitHub\Repos::organisation('propcom')->execute();
 	}
 
 	public function action_404() {
